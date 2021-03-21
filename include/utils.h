cf055a4619b797afe307210184bee694daa14d80
Caio Borghi <caiohoborghi@gmai.com>
2021-03-21 18:20:21 -0300

    coments on utils.h

--- include/utils.h
+++ include/utils.h
@@ -4,11 +4,13 @@
 #include <stdlib.h>
 #include <lexer.h>
 
+// Declaration at lexer.c
 extern int line_number;
 
+// Functions used to transform enum/type into string
 char *get_object_type_label(int object_type);
-
 char *get_var_type_id(int var_type);
 char *get_var_label(int var_type, char *var_name);
 
+// Function to print compilation error
 void show_error(char *message);
